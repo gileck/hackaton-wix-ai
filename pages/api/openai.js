75e15179c73d2dc64860852c1780c14b5838c4d1
Gil Eckstein <gileck@wix.com>
2023-06-01 17:48:00 +0300

    commit

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,7 +1,5 @@
 import { Configuration, OpenAIApi } from "openai"
 
-const apiKey = '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';
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
