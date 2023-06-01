0209cd76ff901b2752b9944ba8fb229b0a8e49a4
Gil Eckstein <gileck@wix.com>
2023-06-01 21:39:32 +0300

    commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,74 +1,91 @@
 import {buildResponse} from './logic'
 
 function returnMockData() {
-  return {
-    result: {
-      original: value,
-      sections: [
-        {
-          "id": "comp-leq228ux",
-          "match": {
-            "text1": "Pad Thai is a popular dish in Thailand, made with stir-fried noodles, vegetables, and a variety of spices",
-            "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-kM84ZsWeMaV73vqGfx5iXXeZ/user-etUJUdBD2zN8qf5eWGOvPECf/img-2j86FkA5EZ60W6JKNLYUJa5F.png?st=2023-06-01T11%3A28%3A56Z&se=2023-06-01T13%3A28%3A56Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T21%3A37%3A23Z&ske=2023-06-01T21%3A37%3A23Z&sks=b&skv=2021-08-06&sig=cx2eWx7eBeAeSF5s2diQXHX2iYSub8KNGJE%2ByDnQnTg%3D",
-            "title": "Best Foods to Eat in Thailand"
-          }
-        },
-        {
-          "id": "comp-leq228ux",
-          "match": {
-            "text1": "India is known for its delicious and diverse cuisine, with dishes ranging from spicy curries to sweet desserts. Popular dishes include tandoori chicken, samosas, and naan bread.",
-            "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-kM84ZsWeMaV73vqGfx5iXXeZ/user-etUJUdBD2zN8qf5eWGOvPECf/img-x2kAhxQ6EydHLFisi9fqICLe.png?st=2023-06-01T11%3A28%3A55Z&se=2023-06-01T13%3A28%3A55Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A33%3A00Z&ske=2023-06-01T20%3A33%3A00Z&sks=b&skv=2021-08-06&sig=75MOvO4fl3SHO7lSkFEHNnXy8%2BK5Ys1Hvw1OxsLD/wg%3D",
-            "title": "Best Foods to Eat in India"
-          }
-        },
-        {
-          "id": "comp-leq228ux",
-          "match": {
-            "text1": "Japan is known for its delicious cuisine, with sushi being one of the most popular dishes",
-            "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-kM84ZsWeMaV73vqGfx5iXXeZ/user-etUJUdBD2zN8qf5eWGOvPECf/img-r2LawM80EvdPvOvYhC0b2OOc.png?st=2023-06-01T11%3A28%3A54Z&se=2023-06-01T13%3A28%3A54Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-01T07%3A16%3A32Z&ske=2023-06-02T07%3A16%3A32Z&sks=b&skv=2021-08-06&sig=M/mkCEQu5df6l0LCWPJaO2IzHGlAa1WSh3Qppvkt1ng%3D",
-            "title": "Best Foods to Eat in Japan"
-          }
-        },
-        {
-          "id": "comp-leq228ux",
-          "match": {
-            "text1": "Tacos al Pastor is a popular Mexican dish made with marinated pork, pineapple, and spices, served on a warm tortilla",
-            "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-kM84ZsWeMaV73vqGfx5iXXeZ/user-etUJUdBD2zN8qf5eWGOvPECf/img-rObuGqfC1gI4iF9ohHUBgbNY.png?st=2023-06-01T11%3A28%3A55Z&se=2023-06-01T13%3A28%3A55Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A31%3A16Z&ske=2023-06-01T20%3A31%3A16Z&sks=b&skv=2021-08-06&sig=quzzgxsehBaLXr9vCbg9n98DCludJh3UztZymHhkXiQ%3D",
-            "title": "Best Foods to Eat in Mexico"
-          }
+    return {
+        "result": {
+            "original": "which cities in europe are the best to visit?",
+            "sections": [{
+                "id": "comp-leq228ux",
+                "match": {
+                    "text1": "Rome is an iconic city in Italy offering plenty of attractions and sites to explore such as the Colosseum, the Trevi Fountain, and the Pantheon.",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-CvipHxeeFFcGCETrJWgXOvBY.png?st=2023-06-01T16%3A24%3A50Z&se=2023-06-01T18%3A24%3A50Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-01T11%3A56%3A05Z&ske=2023-06-02T11%3A56%3A05Z&sks=b&skv=2021-08-06&sig=KXnkDJ5X9gAgtU7J/GYP0nsllBbZiTohPxnVnLhZcnc%3D",
+                    "followups": ["what are the best restaurants in Rome?", "what are the best tourist attractions in Rome?"],
+                    "title": "Rome, Italy"
+                }
+            }, {
+                "id": "comp-leq228ux",
+                "match": {
+                    "text1": "Paris is considered one of the most romantic cities in the world, and is known for its grandiose art, culture, cuisine, and landmarks.",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-v33kRYLBJ7r7rRqc6uDoyBQA.png?st=2023-06-01T16%3A24%3A49Z&se=2023-06-01T18%3A24%3A49Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A30%3A35Z&ske=2023-06-01T20%3A30%3A35Z&sks=b&skv=2021-08-06&sig=U%2B9ZEjkH8Wr6NCE/46w1NSdPKu2TTNszgw83J6HPE4Q%3D",
+                    "followups": ["what are the best restaurants in Paris?", "what are the best attractions in Paris?"],
+                    "title": "Paris, France"
+                }
+            }, {
+                "id": "comp-leq228ux",
+                "match": {
+                    "text1": "London is a vibrant city in England with a rich cultural heritage and lots of attractions to explore",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-rgGOFBMe5s6NNTVor9wQjAhj.png?st=2023-06-01T16%3A24%3A49Z&se=2023-06-01T18%3A24%3A49Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A32%3A11Z&ske=2023-06-01T20%3A32%3A11Z&sks=b&skv=2021-08-06&sig=ZOzHdebLHk2bFJetnILEtT5zkaP1zDABng26C3hjqwE%3D",
+                    "followups": ["what are the best museums in London?", "what are the best bars in London?"],
+                    "title": "London, England"
+                }
+            }, {
+                "id": "comp-leqqmqit",
+                "match": {
+                    "text1": "Barcelona is a vibrant, cosmopolitan city in Spain and is considered one of the most popular cities to visit in Europe. It is filled with interesting museums, restaurants and other attractions",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-KtHERZG4afkvWDk27VtiJt9A.png?st=2023-06-01T16%3A24%3A49Z&se=2023-06-01T18%3A24%3A49Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-01T14%3A49%3A48Z&ske=2023-06-02T14%3A49%3A48Z&sks=b&skv=2021-08-06&sig=V3/g3oiwtdvETK7qYx0eP3AfHY8Kks45osK2mikygZQ%3D",
+                    "followups": ["what attractions are in Barcelona?", "what are the best restaurants in Barcelona?"],
+                    "title": "Barcelona, Spain"
+                }
+            }, {
+                "id": "comp-leq228ux",
+                "match": {
+                    "text1": "Vienna is the cultural and political center of Austria, full of beautiful architecture, great food, and a rich history.",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-ezW7QAEGXG5k4mLLYeqz0VHx.png?st=2023-06-01T16%3A24%3A49Z&se=2023-06-01T18%3A24%3A49Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A31%3A03Z&ske=2023-06-01T20%3A31%3A03Z&sks=b&skv=2021-08-06&sig=g3n5I4dhRtwwulOU/A2SideIgPi4fbRGG0NE2qSqY%2B4%3D",
+                    "followups": ["what is the best time of year to visit Vienna?", "what are the must-see attractions in Vienna?"],
+                    "title": "Vienna, Austria"
+                }
+            }, {
+                "id": "comp-leq228ux",
+                "match": {
+                    "text1": "Amsterdam is the capital of the Netherlands and is filled with culture, art galleries, and canals",
+                    "image1": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ScnAS4Ux3qyBQl103o7zWy0j/user-nBd5rVlHBSuj6jZ8aUjGPDDZ/img-dtpiqZAxkPzGIEdiqA64AwjQ.png?st=2023-06-01T16%3A24%3A51Z&se=2023-06-01T18%3A24%3A51Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-05-31T20%3A33%3A15Z&ske=2023-06-01T20%3A33%3A15Z&sks=b&skv=2021-08-06&sig=y2UHVyIjFSnnshem5PgAd9hhqk9I%2Bw02DsWBbzIVO%2BQ%3D",
+                    "followups": ["what are the best attractions in Amsterdam?", "what are the best cafes in Amsterdam?"],
+                    "title": "Amsterdam, Netherlands"
+                }
+            }]
         }
-      ]
     }
-  }
 }
+
 export default async function (req, res) {
-  const value = req.body.value || '';
+    // return res.status(200).json(returnMockData())
+    const value = req.body.value || '';
 
 
-  if (value.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid value",
-      }
-    });
-    return;
-  }
-
-  try {
-    const result = await buildResponse(value)
-    res.status(200).json({ result})
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
+    if (value.trim().length === 0) {
+        res.status(400).json({
+            error: {
+                message: "Please enter a valid value",
+            }
+        });
+        return;
     }
-  }
+
+    try {
+        const result = await buildResponse(value)
+        res.status(200).json({result})
+    } catch (error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+                error: {
+                    message: 'An error occurred during your request.',
+                }
+            });
+        }
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState } from "react";
+import {useState} from "react";
 import * as React from 'react'
 import styles from "./index.module.css";
 import Grid from "@mui/material/Grid";
@@ -7,166 +7,246 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
+import CircularProgress from '@mui/material/CircularProgress';
+
+import Backdrop  from "@mui/material/Backdrop";
+
 
 const dsUrl = 'https://create.editorx.com/html/editor/web/renderer/render/document/45a425f4-24e4-4966-b8b8-29809d145ef7?metaSiteId=82886fa8-dee5-43a5-afe7-c2f073b53082&editorSessionId=a6868c13-13e1-494d-b82f-b884982cd214&esi=a6868c13-13e1-494d-b82f-b884982cd214&isEdited=true&isSantaEditor=true&dsOrigin=wixwiz&ds=true&forceResponsive=true&configName=responsive&enableScopes=true'
 const urlSuffix = '&isReadOnly=true&disableSave=true&DmSource=https://localhost:8080&debug=dm'
 const dsSrc = `${dsUrl}${urlSuffix}`
 
 export default function Home() {
-  const [valueInput, setValueInput] = useState("");
-  const [documentServicesLoaded, setDocumentServicesLoaded] = useState(false);
-  const [items, setItems] = useState([])
+    const [valueInput, setValueInput] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [documentServicesLoaded, setDocumentServicesLoaded] = useState(false);
+    const [items, setItems] = useState([])
 
-  async function navigate(title) {
-    console.log('Navigate to', title)
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ value: title }),
-      });
+    async function navigate(title) {
+        setLoading(true)
+        console.log('Navigate to', title)
+        try {
+            const response = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({value: title}),
+            });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
 
-      const {result} = data
-      const {sections} = result
+            const {result} = data
+            const {sections} = result
 
-      console.log(sections)
+            console.log(sections)
 
-      const items = sections.map(section => {
-        return {
-            id: section.id,
-            title: section.match.title,
-            description: section.match.text1,
-            image: section.match.image1,
-            followups: section.match.followups,
+            const items = sections.map(section => {
+                return {
+                    id: section.id,
+                    title: section.match.title,
+                    description: section.match.text1,
+                    image: section.match.image1,
+                    followups: section.match.followups,
+                }
+            })
+            setItems(items)
+            setLoading(false)
+
+
+        } catch (error) {
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
+            setLoading(false)
+
         }
-      })
-      setItems(items)
-
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
     }
-  }
 
-  function onFollowupClick(followupQuestion) {
-    setItems([])
-    setValueInput(followupQuestion)
-    navigate(followupQuestion)
-  }
+    function onFollowupClick(followupQuestion) {
+        setItems([])
+        setValueInput(followupQuestion)
+        navigate(followupQuestion)
+    }
 
-  async function onSubmit(event) {
-    setItems([])
-    event.preventDefault();
-    await navigate(valueInput)
-  }
-
-  const CompsById = {
-    'comp-leq228ux': ItemWithImage,
-    'comp-leqqmqit': ItemNoImage,
-  }
+    async function onSubmit(event) {
+        setItems([])
+        event.preventDefault();
+        await navigate(valueInput)
+    }
 
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    return (
 
-      <main className={styles.main}>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="question"
-            placeholder="Ask a question"
-            value={valueInput}
-            onChange={(e) => setValueInput(e.target.value)}
-          />
-          {documentServicesLoaded ? <input type="submit" value="Answer"/> : null}
-        </form>
-        {items ? items.map((item) => {
-          const Comp = CompsById[item.id]
-          return <div style={{width: '80%',margin: '10px'}} key={item.title}>
-            <Comp onFollowupClick={onFollowupClick} post={item}/>
-          </div>
-        }) : ''}
+        <div>
+            <style jsx global>
+                {`
+                  .link:hover {
+                    color: #102457;
+                  }
+
+                  body {
+                    background: #ededed;
+                        font-family: sans-serif;
+                  }
+                `}
+            </style>
+            <Head>
+                <title>OpenAI Quickstart</title>
+                <link rel="icon" href="/dog.png"/>
+            </Head>
+
+            <main className={styles.main}>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        name="question"
+                        placeholder="How Can I Help?"
+                        value={valueInput}
+                        onChange={(e) => setValueInput(e.target.value)}
+                    />
+                    {documentServicesLoaded ? <input type="submit" value="Answer"/> : null}
+                </form>
+
+                {!loading && <Display items={items} onFollowupClick={onFollowupClick}/>}
+
+                {loading && <CircularProgress sx={{color: '#5c5c5c'}} />}
 
 
-      </main>
-    </div>
-  );
+            </main>
+        </div>
+    );
 }
 
+function Display(props) {
+    const {items, onFollowupClick} = props
+
+    const CompsById = {
+        'comp-leq228ux': MediaCard,
+        'comp-leqqmqit': ItemNoImage,
+    }
+
+
+    return (
+        <div style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            width: '80%'
+
+        }}>
+            {items ? items.map((item) => {
+                const Comp = CompsById[item.id]
+                return <div key={item.title}>
+                    <Comp onFollowupClick={onFollowupClick} post={item}/>
+                </div>
+            }) : ''}
+        </div>
+    )
+}
+
+
 function ItemWithImage(props) {
-  const { post } = props;
+    const {post} = props;
 
-
-  return (
-        <Grid item key={post.title}>
-          <Card>
-            <Card sx={{ display: 'flex' }}>
-              <CardContent sx={{ flex: 1 }}>
-                <Typography component="h2" variant="h5">
-                  {post.title}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                  {post.description}
-                </Typography>
-                <Typography variant="subtitle1" paragraph>
-                </Typography>
-                <Typography variant="subtitle1" color="primary">
-                  {post.followups && post.followups.map(q => <div key={q}>
-                    <Typography onClick={() => props.onFollowupClick(q)} component={'a'} href={'#'}> {q} </Typography>
-                  </div>)}
-                </Typography>
-              </CardContent>
-              <CardMedia
-                  component="img"
-                  sx={{ width: '30%', display: { xs: 'none', sm: 'block' } }}
-                  image={post.image}
-              />
+//style={{width: '80%', margin: '10px'}}
+    return (
+        <Card style={{border: 'none'}}>
+            <Card sx={{display: 'flex'}}>
+                <CardContent sx={{flex: 1}}>
+                    <Typography component="h2" variant="h5">
+                        {post.title}
+                    </Typography>
+                    <Typography variant="subtitle1" paragraph>
+                        {post.description}
+                    </Typography>
+                    <Typography variant="subtitle1" paragraph>
+                    </Typography>
+                    <Typography variant="subtitle1" color="primary">
+                        {post.followups && post.followups.map(q => <div key={q}>
+                            <Typography onClick={() => props.onFollowupClick(q)} component={'a'}
+                                        href={'#'}> {q} </Typography>
+                        </div>)}
+                    </Typography>
+                </CardContent>
+                <CardMedia
+                    component="img"
+                    sx={{width: '30%', display: {xs: 'none', sm: 'block'}}}
+                    image={post.image}
+                />
             </Card>
-          </Card>
-        </Grid>
-  );
+        </Card>
+    );
 }
 
 function ItemNoImage(props) {
-  const { post } = props;
+    const {post} = props;
 
 
-  return (
-      <Grid item key={post.title}>
-        <Card>
-          <Card sx={{ display: 'flex' }}>
-            <CardContent sx={{ flex: 1 }}>
-              <Typography component="h2" variant="h5">
-                {post.title}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {post.description}
-              </Typography>
-              <Typography variant="subtitle1" color="primary">
-                {post.followups && post.followups.map(q => <div key={q}>
-                  <Typography onClick={() => props.onFollowupClick(q)} component={'a'} href={'#'}> {q} </Typography>
-                </div>)}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Card>
-      </Grid>
-  );
+    return (
+        <div style={{height: '200px', padding: '15px'}}>
+            <Grid item key={post.title}>
+                <div>
+                    <Card sx={{display: 'flex', boxShadow: 'none'}}>
+                        <CardContent sx={{flex: 1}}>
+                            <Typography component="h2" variant="h5">
+                                {post.title}
+                            </Typography>
+                            <Typography variant="subtitle1" paragraph>
+                                {post.description}
+                            </Typography>
+                            <Typography variant="subtitle1" color="primary">
+                                {post.followups && post.followups.map(q => <div key={q}>
+                                    <div className='link' style={{color: '#8ab4f8', cursor: "pointer"}}>
+                                        {q}
+                                    </div>
+                                </div>)}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+            </Grid>
+        </div>
+    );
+}
+
+function MediaCard({post, onFollowupClick}) {
+    return (
+        <div style={{padding: '15px'}}>
+            <Card sx={{maxWidth: 345, height: 500, boxShadow: 'none'}}>
+                <CardMedia
+                    sx={{height: 200}}
+                    image={post.image}
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {post.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {post.description}
+                    </Typography>
+                    <div style={{marginTop: '20px'}}>
+                        <Typography variant="body3" color="text.secondary">
+                            {post.followups && post.followups.map(q => <div key={q}>
+                                <div className='link' style={{color: '#8ab4f8', cursor: "pointer"}}>
+                                    {q}
+                                </div>
+                            </div>)}
+                        </Typography>
+                    </div>
+                </CardContent>
+
+            </Card>
+        </div>
+    );
 }
